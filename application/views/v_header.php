5b88aa1f3f8bd2beb2260dc3593895d10002e763
rzdnv <113969193+rzdnv@users.noreply.github.com>
2022-10-04 14:35:50 +0700

    merubah title

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Web Prog II | Merancang Template sederhana dengan codeigniter</title>
+    <title>Latihan | Merancang Template sederhana dengan codeigniter</title>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/stylebuku.css">
     <link rel="shortcut icon" href="<?php echo base_url() ?>assets/img/statue-of-liberty-icon.png">
 </head>
